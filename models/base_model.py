b6a5203499c6efb411a4afd999ac36fa5e0c9379
AaronEGH16 <aezegonz@gmail.com>
2023-10-23 21:07:43 +0000

    fixed datatime at created instance

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """should print [<class name>] (<self.id>) <self.__dict__>
