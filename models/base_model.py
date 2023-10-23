c9b3c671c25d85e99add7ada017f9a836b3a72eb
seba17962 <isebsebastiansilva@gmail.com>
2023-10-23 14:55:02 +0000

    add docstring

--- models/base_model.py
+++ models/base_model.py
@@ -12,10 +12,15 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
+        """should print [<class name>] (<self.id>) <self.__dict__>
+        """
         print(f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
+        """updates the public instance attribute
+        updated_at with the current datetime"""
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        
+        """ returns a dictionary containing all
+        keys/values of __dict__ of the instance"""
