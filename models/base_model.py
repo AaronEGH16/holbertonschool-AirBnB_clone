45db41f5423811b1da9eddca4f4542c5019ae131
seba17962 <isebsebastiansilva@gmail.com>
2023-10-25 16:57:39 +0000

    deleted the self in methods fo storage save and new

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new()
+            storage.new(self)
 
     def __str__(self):
         """should print [<class name>] (<self.id>) <self.__dict__>
