e01711cb804f95ef132785c13e206d6239ce9f61
seba17962 <isebsebastiansilva@gmail.com>
2023-10-25 16:24:38 +0000

    add doctring

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,13 +10,16 @@ class FileStorage():
     __objects = {}
 
     def all(self):
+        """Returns the __objects dictionary."""
         return FileStorage.__objects
 
     def new(self, obj):
+        """Adds an object to __objects with the key <class name>.id."""
         key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
 
     def save(self):
+        """Serializes __objects to a JSON file at __file_path."""
         serialized_objects = {}
         for key, obj in FileStorage.__objects.items():
             serialized_objects[key] = obj.to_dict()
@@ -24,6 +27,10 @@ class FileStorage():
             json.dump(serialized_objects, file)
 
     def reload(self):
+        """Deserializes the JSON file at __file_path to __objects
+        (only if the JSON file (__file_path) exists; otherwise,
+        do nothing. If the file doesn't exist,
+        no exception should be raised)."""
         try:
             with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
                 data = json.load(file)
