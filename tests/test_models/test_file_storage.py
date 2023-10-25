e7d77d8070f770494154d591aabbcf6e3d866d49
seba17962 <isebsebastiansilva@gmail.com>
2023-10-25 23:49:41 +0000

    test file storage

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+this module contains all test of file_storage
+"""
+import unittest
+import os
+from datetime import datetime
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class Tests_FileStorage(unittest.TestCase):
+    """
+    tests of FileStorage class:
+        - test 'all'
+        - test 'new'
+        - test 'save'
+        - test 'reload'
+    """
+
+    def setUp(self):
+        self.storage = FileStorage()
+        self.base_model = BaseModel()
+        self.file_path = "file.json"
+
+    def tearDown(self):
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
+
+    def test_all(self):
+        self.assertIsInstance(self.storage.all(), dict)
+
+    def test_new(self):
+        self.storage.new(self.base_model)
+        obj_key = f"{self.base_model.__class__.__name__}.{self.base_model.id}"
+        self.assertTrue(obj_key in self.storage.all())
+
+    def test_save(self):
+        self.storage.new(self.base_model)
+        self.storage.save()
+        obj_key = f"{self.base_model.__class__.__name__}.{self.base_model.id}"
+        self.assertTrue(obj_key in self.storage.all())    
+
+    def test_reload(self):
+        self.storage.new(self.base_model)
+        self.storage.save()
+
+        new_storage = FileStorage()
+        new_storage.reload()
+
+        obj_key = f"{self.base_model.__class__.__name__}.{self.base_model.id}"
+        self.assertTrue(obj_key in new_storage.all())
+
+
