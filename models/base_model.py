576d5386c52d56125fcef1714d4bd93eb29aa587
seba17962 <isebsebastiansilva@gmail.com>
2023-10-25 16:35:06 +0000

    add storage methods save in save and new in __init__

--- models/base_model.py
+++ models/base_model.py
@@ -33,6 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """should print [<class name>] (<self.id>) <self.__dict__>
@@ -43,6 +44,7 @@ class BaseModel:
         """updates the public instance attribute
         updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save(self)
 
     def to_dict(self):
         """ returns a dictionary containing all
