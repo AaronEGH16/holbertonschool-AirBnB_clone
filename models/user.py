8d414542e7b6a8b46dca3db14848ac057b27fb94
seba17962 <isebsebastiansilva@gmail.com>
2023-10-26 13:39:05 +0000

    add public class attributes to user

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""classs user that define the user attributes
+"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """User class for the database."""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
