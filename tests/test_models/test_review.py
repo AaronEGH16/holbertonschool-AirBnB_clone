3359b0b97dcd4cd0c711b219b5adc42caa884720
seba17962 <isebsebastiansilva@gmail.com>
2023-10-28 14:58:15 +0000

    test review

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,20 @@
+import unittest
+from models.review import Review
+from models.base_model import BaseModel
+
+class Test_Review(unittest.TestCase):
+    """Tests:
+            test its created all atribute class
+            test review inherit BaseModel
+    
+    """
+    def test_review_attributes(self):
+        review = Review()
+        self.assertEqual(review.place_id, "")
+        self.assertEqual(review.user_id, "")
+        self.assertEqual(review.text, "")
+
+    def test_review_inheritance(self):
+        review = Review()
+        self.assertTrue(isinstance(review, BaseModel))
+    
