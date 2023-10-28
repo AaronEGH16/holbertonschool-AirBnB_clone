67773b357fbddc52b93bdcabd00ae59ec164462f
seba17962 <isebsebastiansilva@gmail.com>
2023-10-28 14:05:14 +0000

    add the class amenity

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""classs Amenity that define the amenity attributes
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Define the class Amenity"""
+    name = ""
