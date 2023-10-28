017877aef8fd9f64ae5737e870f63ea7e4f4012a
seba17962 <isebsebastiansilva@gmail.com>
2023-10-28 14:03:56 +0000

    add the class state

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""classs state that define the state attributes
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Define the class Satate"""
+    name = ""
