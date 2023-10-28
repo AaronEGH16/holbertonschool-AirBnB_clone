58a42896f9660813e9cbdd7cc4fafba040ca8a5a
seba17962 <isebsebastiansilva@gmail.com>
2023-10-28 15:58:59 +0000

    add the classes to woork in console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,11 @@ using a dictionary representation
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class FileStorage():
@@ -12,7 +17,8 @@ class FileStorage():
     and deserializes JSON file to instances"""
     __file_path = "file.json"
     __objects = {}
-    objclass = {"BaseModel": BaseModel, "User": User}
+    objclass = {"BaseModel": BaseModel, "User": User, "Amenity": Amenity,
+                "City": City, "Place": Place, "Review": Review, "State": State}
 
     def all(self):
         """Returns the __objects dictionary."""
