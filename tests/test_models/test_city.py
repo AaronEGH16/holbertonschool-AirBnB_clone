48309432de59318adb1bfa506d29690ba9917061
seba17962 <isebsebastiansilva@gmail.com>
2023-10-28 14:44:55 +0000

    add the unittest to class city

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Test state"""
+import unittest
+from models.city import City
+from models.base_model import BaseModel
+
+
+class Test_city(unittest.TestCase):
+    """Tests:
+            state_id and name its created
+            City inherit BaseModel
+    """
+    def test_city_attributes(self):
+        city = City()
+        self.assertEqual(city.state_id, "")
+        self.assertEqual(city.name, "")
+
+    def test_city_inheritance(self):
+        city = City()
+        self.assertTrue(isinstance(city, BaseModel))
+        
