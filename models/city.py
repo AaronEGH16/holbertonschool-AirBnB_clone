9775448c55b42412ccfb398dff790157fe827949
seba17962 <isebsebastiansilva@gmail.com>
2023-10-28 14:03:40 +0000

    add the class city

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""classs city that define the city attributes
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Define the class City"""
+    state_id = ""
+    name = ""
