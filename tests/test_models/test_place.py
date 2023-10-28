edf24dd35ea21d1395b908167248bc9b5087db50
seba17962 <isebsebastiansilva@gmail.com>
2023-10-28 14:55:42 +0000

    test place

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,27 @@
+import unittest
+from models.place import Place
+from models.base_model import BaseModel
+
+class TestPlace(unittest.TestCase):
+    """Test:
+            test its created all atribute class
+            place inherit BaseModel
+    """
+
+    def test_place_attributes(self):
+        place = Place()
+        self.assertEqual(place.city_id, "")
+        self.assertEqual(place.user_id, "")
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
+        self.assertEqual(place.amenity_ids, [])
+
+    def test_place_inheritance(self):
+        place = Place()
+        self.assertTrue(isinstance(place, BaseModel))
